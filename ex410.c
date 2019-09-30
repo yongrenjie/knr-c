b3ff2f0c083cb4f725e15ade042de25ed6f645a0
Jonathan Yong <yongrenjie@gmail.com>
2019-09-30 18:19:59 +0100

    ex410 30-9-19

--- ex410.c
+++ ex410.c
@@ -9,12 +9,13 @@
 #define NUMBER '0' /* signal that a number was found */
 #define VARIABLE '1'
 #define OTHER_OPERATION '2'
+#define END_OF_LINE '3'
 
 int nopop = 0;
 double most_recent;
 double var[26];
 
-int getop(char []);
+int getop(char [], char []);
 void push(double);
 double pop(void);
 void print_top(void);
@@ -22,7 +23,8 @@ void duplicate_top(void);
 void swap_top_two(void);
 void clear_stack(void);
 void print_stack(void);
-void ungets(char s[]);
+
+#define MAXLINE 100
 
 /* reverse Polish calculator */
 
@@ -30,117 +32,123 @@ int main(void)
 {
     int type;
     double op2;
+    char l[MAXLINE];
     char s[MAXOP];
     int var_no;
     double var_val;
+    int len;
 
     printf("define lowercase single-letter variables with \"a 2 def\";\n");
     printf("the variable \"A\" stores the last shown answer\n");
 
-    while((type = getop(s)) != EOF)
+    while((len = my_getline(l, MAXLINE)) != EOF)
     {
-        switch(type)
+        while((type = getop(l, s) != END_OF_LINE))
         {
-            case NUMBER:
-                push(atof(s));
-                break;
-            case VARIABLE:
-                if (islower(s[0])) {
-                    var_no = (s[0] - 'a');
-                    push(var[var_no]);
-                }
-                else if (s[0] == 'A') {
-                    push(most_recent);
-                }
-                break;
-            case OTHER_OPERATION:
-                if (!(strcmp(s, "sin"))) { // strcmp returns 0 if the strings match
-                    push(sin(pop()));
-                }
-                else if (!(strcmp(s, "cos"))) {
-                    push(cos(pop()));
-                }
-                else if (!(strcmp(s, "tan"))) {
-                    push(tan(pop()));
-                }
-                else if (!(strcmp(s, "exp"))) {
-                    push(exp(pop()));
-                }
-                else if (!(strcmp(s, "pow"))) {
+            printf("type is %d\n", type);
+            switch(type)
+            {
+                case NUMBER:
+                    push(atof(s));
+                    break;
+                case VARIABLE:
+                    if (islower(s[0])) {
+                        var_no = (s[0] - 'a');
+                        push(var[var_no]);
+                    }
+                    else if (s[0] == 'A') {
+                        push(most_recent);
+                    }
+                    break;
+                case OTHER_OPERATION:
+                    if (!(strcmp(s, "sin"))) { // strcmp returns 0 if the strings match
+                        push(sin(pop()));
+                    }
+                    else if (!(strcmp(s, "cos"))) {
+                        push(cos(pop()));
+                    }
+                    else if (!(strcmp(s, "tan"))) {
+                        push(tan(pop()));
+                    }
+                    else if (!(strcmp(s, "exp"))) {
+                        push(exp(pop()));
+                    }
+                    else if (!(strcmp(s, "pow"))) {
+                        op2 = pop();
+                        push(pow(pop(), op2));
+                    }
+                    else if (!(strcmp(s, "sqrt"))) {
+                        push(sqrt(pop()));
+                    }
+                    else if (!(strcmp(s, "def"))) {
+                        double var_val = pop();
+                        var[var_no] = var_val;
+                        pop();
+                        /* when calling "a 2 def" for example, 'a' pushes the value of a to the stack
+                         * then 2 is pushed, so overall we need to pop 2 off (first line) and then pop
+                         * the original value of a off as well. */
+                        // Because of initialisation, any undefined variables have a value of 0.
+                        nopop = 1;
+                    }
+                    else {
+                        printf("error: unknown command %s\n", s);
+                    }
+                    break;
+                case END_OF_LINE:
+                    if (!nopop) {
+                        most_recent = pop();
+                        printf("\t%.8g\n", most_recent);
+                    }
+                    nopop = 0;
+                    break;
+                case '+':
+                    push(pop() + pop());
+                    break;
+                case '*':
+                    push(pop() * pop());
+                    break;
+                case '-':
                     op2 = pop();
-                    push(pow(pop(), op2));
-                }
-                else if (!(strcmp(s, "sqrt"))) {
-                    push(sqrt(pop()));
-                }
-                else if (!(strcmp(s, "def"))) {
-                    double var_val = pop();
-                    var[var_no] = var_val;
-                    pop();
-                    /* when calling "a 2 def" for example, 'a' pushes the value of a to the stack
-                     * then 2 is pushed, so overall we need to pop 2 off (first line) and then pop
-                     * the original value of a off as well. */
-                    // Because of initialisation, any undefined variables have a value of 0.
+                    push(pop() - op2);
+                    break;
+                case '/':
+                    op2 = pop();
+                    if(op2 != 0.0)
+                        push(pop() / op2);
+                    else
+                        printf("error: zero divisor\n");
+                    break;
+                case '%':
+                    op2 = pop();
+                    if (op2 != 0.0)
+                        push(((int) pop()) % ((int) op2));
+                    else
+                        printf("error: zero divisor\n");
+                    break;
+                case '@':
+                    print_top();
                     nopop = 1;
-                }
-                else {
+                    break;
+                case '#':
+                    duplicate_top();
+                    nopop = 1;
+                    break;
+                case '$':
+                    swap_top_two();
+                    nopop = 1;
+                    break;
+                case '^':
+                    clear_stack();
+                    nopop = 1;
+                    break;
+                case '!':
+                    print_stack();
+                    nopop = 1;
+                    break;
+                default:
                     printf("error: unknown command %s\n", s);
-                }
-                break;
-            case '+':
-                push(pop() + pop());
-                break;
-            case '*':
-                push(pop() * pop());
-                break;
-            case '-':
-                op2 = pop();
-                push(pop() - op2);
-                break;
-            case '/':
-                op2 = pop();
-                if(op2 != 0.0)
-                    push(pop() / op2);
-                else
-                    printf("error: zero divisor\n");
-                break;
-            case '%':
-                op2 = pop();
-                if (op2 != 0.0)
-                    push(((int) pop()) % ((int) op2));
-                else
-                    printf("error: zero divisor\n");
-                break;
-            case '\n':
-                if (!nopop) {
-                    most_recent = pop();
-                    printf("\t%.8g\n", most_recent);
-                }
-                nopop = 0;
-                break;
-            case '@':
-                print_top();
-                nopop = 1;
-                break;
-            case '#':
-                duplicate_top();
-                nopop = 1;
-                break;
-            case '$':
-                swap_top_two();
-                nopop = 1;
-                break;
-            case '^':
-                clear_stack();
-                nopop = 1;
-                break;
-            case '!':
-                print_stack();
-                nopop = 1;
-                break;
-            default:
-                printf("error: unknown command %s\n", s);
-                break;
+                    break;
+            }
         }
     }
 
@@ -226,93 +234,76 @@ void print_stack(void)
 
 #include <ctype.h>
 
-int getch(void);
-void ungetch(int);
-
-/* getop: get next operator or numeric operand */
-int getop(char s[])
+int getop(char f[], char s[])
 {
-    int i, c;
+    // getop parses the string f (f should be obtained from my_getline()) and
+    // places the first operator / operand into s. It then needs to slice off
+    // s from the beginning of f.
 
-    while((s[0] = c = getch()) == ' ' || c == '\t')
-        ;
-
-    s[1] = '\0';
-
+    int i; // the index of f
+    char c; // the character being read
     i = 0;
+    while ((c = f[i]) == ' ' || c == '\t') i++ ; // skip whitespace
 
-    if (isalpha(c)) {
-        while (isalpha(s[++i] = c = getch()))
-            ;
-        if (c != EOF){
-            ungetch(c);
-        }
-        s[i] = '\0';
-        if (i == 1) {
-            return VARIABLE;
-        }
-        else {
-            return OTHER_OPERATION;
-        }
+    int j; // index of s being written to
+    j = 0;
+    while ((c = f[i]) != ' ' && c != '\t' && c != '\n' && c != '\0' && c != EOF) {
+        s[j] = c;
+        i++;
+        j++;
+    }
+    s[j] = '\0'; // terminate the string
+
+    int k; // the index of f being overwritten
+    k = 0;
+    while ((c = f[i]) != '\0') {
+        f[k] = c;
+        i++;
+        k++;
+    }
+    f[k] = '\0'; // terminate the overwritten string
+
+    // now f and s should be set correctly
+    // we now need to determine what value getop() should return to the main routine
+
+    printf("s is: %s\n", s);
+    printf("strlen(s) is: %d\n", strlen(s));
+    
+    // TODO: need to suppress the extra output when sending an EOF 
+
+    // if s is empty, it means we reached the end of the line
+    if (strlen(s) == 0) return END_OF_LINE;
+
+    // if s is an alphabetical string
+    if (isalpha(s[0])) {
+        printf("isalpha\n");
+        if (strlen(s) == 1) return VARIABLE;
+        else return OTHER_OPERATION;
     }
 
-    if(!isdigit(c) && c != '.' && c != '+' && c != '-'){
-        return c; /* not a number, or a sign */
+    // if s is an operator (one character long...)
+    if (strlen(s) == 1) {
+        if (isdigit(s[0])) return NUMBER;
+        else return s[0];
     }
-
-    int sign;
-    if ((sign = c) == '-' || c == '+'){
-        s[++i] = c = getch();
-        if(!isdigit(c) && c != '.'){
-            s[i] = '\0';
-            ungetch(c);
-            return sign; /* not a number */
-        }
-        else {
-            if(isdigit(c)) /* collect integer part */
-                while(isdigit(s[++i] = c = getch()))
-                    printf("debug2");
-            if(c == '.')
-                while(isdigit(s[++i] = c = getch()))
-                    ;
-        }
-    }
-    else {
-        if(isdigit(c)) /* collect integer part */
-            while(isdigit(s[++i] = c = getch()))
-                ;
-        if(c == '.')
-            while(isdigit(s[++i] = c = getch()))
-                ;
-    }
-    s[i] = '\0';
-    if(c != EOF)
-        ungetch(c);
+    
+    printf("isa\n");
+    // otherwise it's a number
     return NUMBER;
 }
 
-void ungets(char s[])
+int my_getline(char s[], int lim)
 {
-    for (int i = strlen(s) - 1; i >= 0; --i) {
-        ungetch(s[i]);
+    int c, i;
+    for (i = 0; ((c = getchar()) != EOF && c != '\n'); ++i) {
+        if (i < lim - 1) {
+            s[i] = c;
+        }
     }
-}
-
-
-#define BUFSIZE 100
-
-char buf[BUFSIZE]; /* buffer for ungetch */
-int bufp = 0; /* next free position in buf */
-
-int getch(void) /* get a (possibly pushed back) character */
-{
-    return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-void ungetch(int c) /* push character back on input */
-{
-    if(bufp >= BUFSIZE)
-        printf("ungetch: too many characters\n");
-    else
-        buf[bufp++] = c;
+    if ((c == '\n') && (i < lim - 1)) {
+        s[i] = c;
+        ++i;
+    }
+    s[i <= lim - 1 ? i : lim - 1] = '\0';
+    return i;
 }
