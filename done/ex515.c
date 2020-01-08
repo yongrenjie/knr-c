d457ac7fa6683d2822bae26d05bf6dbc80c14716
Jonathan Yong <yongrenjie@gmail.com>
2020-01-08 23:23:13 +0000

    Fix bug in Ex 5.15
    My implementation of strcmp would not return 0 if both strings had
    ended. Instead it would read one past the end of the array and compare
    those two values. The code still works regardless of what value this
    returns, but it technically wasn't correctly coded.

--- done/ex515.c
+++ done/ex515.c
@@ -90,6 +90,7 @@ int numcmp (const char *s1, const char *s2)
 int istrcmp (const char *s1, const char *s2)
 {
     while (tolower(*s1) == tolower(*s2)) {
+        if (*s1 == '\0') return 0;
         s1++;
         s2++;
     }
